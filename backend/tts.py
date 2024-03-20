9e80229f1f35ea35ce667eb2271d715ae16230f0
Jordan Dyckes <54044028+J-DRD@users.noreply.github.com>
2024-03-20 13:57:51 +0000

    tweak

--- backend/tts.py
+++ backend/tts.py
@@ -64,7 +64,7 @@ def _elevenlabs_to_speech(text, background_tasks):
         api_key=ELEVENLABS_API_KEY,
         text=text,
         voice=ELEVENLABS_VOICE,
-        model="eleven_monolingual_v1"
+        model="eleven_multilingual_v2"
     )
 
     filepath = f"/tmp/{uuid.uuid4()}.mp3"
